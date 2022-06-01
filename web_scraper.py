51630d3bd307614cf2ce22c9c536bd6c516decae
nitishvijai <nitishv@umich.edu>
2022-05-31 23:36:10 -0400

    End of day commit - started Indeed web scraping

--- web_scraper.py
+++ web_scraper.py
@@ -3,7 +3,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-service = Service("../../chromedriver/stable/chromedriver")
-driver = webdriver.Chrome(service=service)
-driver.get("https://www.google.com")
-driver.close()
+# Set up web scraper
+def init_scraper(query):
+    service = Service("../../chromedriver/stable/chromedriver")
+    driver = webdriver.Chrome(service=service)
+    return driver
+
+def process_search():
+    title = 'SmartApply Job Search'
+    print('-' * len(title) + '\n' + title + '\n' + '-' * len(title))
+    query = input("What jobs do you want to look for today? > ")
+    encoded = query.replace(' ', '%20')
+    return encoded
+
+def scrape_indeed(driver, query):
+    driver.get(f"https://www.indeed.com/jobs?q={query}")
+    print("Scraping Indeed...\n")
+    postings = driver.find_elements(By.CLASS_NAME, "cardOutline")
+    
+    for posting in postings:
+        title = posting.find_element(By.CLASS_NAME, "jcs-JobTitle").find_element(By.TAG_NAME, "span").text
+        company = posting.find_element(By.CLASS_NAME, "companyOverviewLink").text
+        print(title)
+        print(company)
+        print()
+
+
+if __name__ == "__main__":
+    query = process_search()
+    driver = init_scraper(query)
+    scrape_indeed(driver, query)
+    
+    driver.close()
