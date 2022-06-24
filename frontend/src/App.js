8812698410b0c0302cb6f9d27c91a566430c784a
nitishvijai <nitishv@umich.edu>
2022-06-23 23:48:37 -0400

    Update homepage with gradient background and minor UI tweaks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div class="logo-placeholder">
+          <h1>SmartApply ✅</h1>
+          <h2>Apply smarter, not harder</h2>
+        </div>
         Search for a job to begin
-        <input type="text" placeholder="Enter job title here..."></input>
-        <button type="submit">Search</button>
+        <div class="search-container">
+          <input type="text" placeholder="Enter job title here..."></input>
+          <button type="submit">Search</button>
+        </div>
       </header>
-      SmartApply v1.0 Alpha. Last updated on 6/20/2022.
+      SmartApply v1.0 Alpha. Last updated on 6/23/2022.
     </div>
   );
 }
