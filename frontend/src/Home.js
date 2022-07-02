c0ae27c5de423c1f1cdd2cf5129c9d58d7185f00
nitishvijai <nitishv@umich.edu>
2022-07-02 12:38:34 -0400

    Enable input validation on search and opening results page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    function submitApp() {
+        const search = document.getElementById("search").value;
+    
+        if (search === "") {
+            alert("Please enter a job title in the search box.");
+        }
+        else {
+            // open the results page with search query
+            console.log("Opening results...");
+            navigate("/search", {state: "Software Engineer"});
+        }
+    }
+
     return (
         <div className="background">
             <header className="App-header">
@@ -11,8 +27,8 @@ const Home = () => {
                 </div>
                 Search for a job to begin
                 <div class="search-container">
-                    <input type="text" placeholder="Enter job title here..."></input>
-                    <button type="submit">Search</button>
+                    <input id="search" type="text" placeholder="Enter job title here..."></input>
+                    <button type="submit" onClick={submitApp}>Search</button>
                 </div>
             </header>
             SmartApply v1.0 Alpha. Last updated on 6/26/2022.
--- frontend/src/results.js
+++ frontend/src/results.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Results = () => {
+    const location = useLocation();
+    let jobTitle = location.state;
+
     return (
         <div className="background">
             <header className="search-header">
                 <div class="logo-placeholder results">
                     <h1 class="logo left-align">SmartApply ✅</h1>
                     <div class="search-container left-align">
-                        <input type="text" placeholder="Enter job title here..."></input>
+                        <input type="text" placeholder="Enter job title here..." value={jobTitle}></input>
                         <button type="submit">Search</button>
                     </div>
                 </div>
